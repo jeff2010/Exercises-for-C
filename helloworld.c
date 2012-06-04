2048e1b11cdce3fb2028c5997f91e73729c86b67
jeff nie <jeff.nie.2009@gmail.com>
2012-06-04 17:24:03 +0800

    add helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.c
+ *
+ *    Description:  helloworld程序
+ *
+ *        Version:  1.0
+ *        Created:  2012年06月04日 17时23分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jeff Nie (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello world.\n");
+	return 0;
+}
